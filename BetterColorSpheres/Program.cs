b68acb255546e4abbedd0617f8c7925a29e70243
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-12 16:25:32 +0100

    add BetterColorSpheres to sln

---
+++ BetterColorSpheres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BetterColorSpheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
