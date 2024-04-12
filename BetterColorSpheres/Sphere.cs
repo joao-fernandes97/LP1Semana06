e1d1c4e637fc1cacf30d4b0f4ecd3a0fbea1948e
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-12 17:04:10 +0100

    Sphere.cs creation

---
+++ BetterColorSpheres/Sphere.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BetterColorSpheres
+{
+    public class Sphere
+    {
+        public Color Color {get;}
+        private int radius;
+        private int timesThrown;
+    }
+}
