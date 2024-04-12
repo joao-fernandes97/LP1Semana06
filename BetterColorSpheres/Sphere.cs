430d15f4a7ae08f5217372f0c18c68e53e1318da
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-12 20:20:15 +0100

    Sphere property usage

--- BetterColorSpheres/Sphere.cs
+++ BetterColorSpheres/Sphere.cs
@@ -6,6 +6,31 @@ namespace BetterColorSpheres
     {
         public Color Color {get;}
         private int radius;
+        public int Radius {get;}
         private int timesThrown;
+        public int TimesThrown {get;}
+        
+
+        public Sphere(Color color, int radius)
+        {
+            Color = color;
+            this.radius = radius;
+            timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            radius = 0;
+        }
+
+        public void Throw()
+        {
+            if(radius>0) timesThrown++;
+        }
+
+        public bool IsPopped()
+        {
+            return radius==0;
+        }
     }
 }
