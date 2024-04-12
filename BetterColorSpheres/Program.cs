b0b945769a1f6aa9007ff8e5fb4cbf8c08cdecfc
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-12 20:28:45 +0100

    Program update using properties

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -6,7 +6,44 @@ namespace BetterColorSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sphere[] spheres = new Sphere[10];
+            Random rand = new Random(5336);
+
+            for (int i = 0; i < spheres.Length; i++)
+            {
+                byte red = (byte)rand.Next(byte.MaxValue+1);
+                byte green = (byte)rand.Next(byte.MaxValue+1);
+                byte blue = (byte)rand.Next(byte.MaxValue+1);
+                
+                Color color = new Color(red,green,blue);
+                spheres[i] = new Sphere(color, rand.Next());
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                int j = rand.Next(spheres.Length);
+                int d20 = rand.Next(1,21);
+
+                if(d20==1) spheres[j].Pop();
+                else spheres[j].Throw();
+            }
+
+            for (int i = 0; i < spheres.Length; i++)
+            {
+                if (spheres[i].IsPopped())
+                {
+                    Console.WriteLine($"Sphere {i} was popped. " + 
+                    $"It was thrown {spheres[i].TimesThrown} times");
+                }else
+                {
+                    Console.WriteLine($"Sphere {i} was thrown " + 
+                    $"{spheres[i].TimesThrown} times. " +
+                    $"Color code: ({spheres[i].Color.Red}, " +
+                    $"{spheres[i].Color.Green}, " +
+                    $"{spheres[i].Color.Blue}, " +
+                    $"{spheres[i].Color.Alpha})");
+                }
+            }
         }
     }
 }
